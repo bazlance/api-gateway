b8f8130328fe149d61cc36ce9c782c219c05c25b
bazlance <krisnu.baz@gmail.com>
2026-08-06 21:06:16 +0700

    Add @POST login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,12 +6,12 @@ import {
   Patch,
   Param,
   Delete,
-  Req,
   Headers,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('users')
 export class UsersController {
@@ -20,6 +20,11 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() body: LoginDto) {
+    return this.usersService.login(body);
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { BadGatewayException, HttpException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class UsersService {
@@ -26,6 +27,28 @@ export class UsersService {
       return data;
     } catch (error) {
       const err = error as AxiosError;
+
+      throw new HttpException(
+        err.response?.data ?? 'Unknown error',
+        err.response?.status ?? 500,
+      );
+    }
+  }
+
+  async login(body: LoginDto) {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.post('http://localhost:3000/user/login', body),
+      );
+
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      if (!err.response) {
+        console.log('err', err);
+        throw new BadGatewayException('User service is unavailable');
+      }
 
       throw new HttpException(
         err.response?.data ?? 'Unknown error',
